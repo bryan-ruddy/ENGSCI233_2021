d844cd5475c8e2c2e94a40819824c2f876c2428b
Bryan Ruddy <b.ruddy@auckland.ac.nz>
2021-03-07 18:41:41 +1300

    Update error233.py
    Add Colab version of exponential example

--- error/error233.py
+++ error/error233.py
@@ -42,6 +42,69 @@ if 'google.colab' in str(get_ipython()):
         # Configure callbacks and go!
         for w in items:
           w.observe(on_value_change, names='value')
+    def exponential_example(delta_e):
+        # Create and display widget layout
+        items = [IntSlider(value = 1, description='terms', min=1, max = 20, step=1)]
+        items += [Dropdown(options = ['half','single','double'], value = 'single')]
+        col_layout=Layout(display='flex', flex_flow='row-wrap', width="100%", justify_content='flex-end', align_items='center')
+        big_widget = HBox(items, layout=col_layout)
+        out_widget = Output()
+        display(big_widget,out_widget)
+        with out_widget:
+            print('Try me!')
+
+        # Callback function for updates
+        def on_value_change(change):
+            precision = items[1].value
+            if precision == 'half':
+                prec = np.float16
+            elif precision == 'single':
+                prec = np.float32
+            else:
+                prec = np.float64
+            # grab current term calculation from appropriate widget
+            terms = items[0].value
+            # calculation
+            # -----------
+                # initialize term calculation
+            sum_terms0 = prec(0.)
+            final_term = prec(0.)		
+                # iterate over number of terms
+            for i in range(0,terms):
+                final_term = delta_e(i, prec)
+                sum_terms0 += final_term
+                # compute and append final term
+            final_term = delta_e(terms, prec)
+            sum_terms = sum_terms0 + final_term
+            # update annotations
+            pr = '{:51.50f}'
+            e = '2.71828182845904523536028747135266249775724709369995'
+            rd = Decimal(pr.format(sum_terms))-(Decimal(pr.format(final_term))+Decimal(pr.format(sum_terms0)))
+            if rd < 0:
+                pad1 = ' '
+            else:
+                pad1 = '  '
+            if terms < 10:
+                pad2 = ' '
+                pad3 = ' '
+            elif terms == 10:
+                pad2 = ' '
+                pad3 = ''
+            else:
+                pad2 = ''
+                pad3 = ''
+
+            out_widget.clear_output()
+            with out_widget:
+                print(pad2+r'          e_'+'{:d}'.format(terms-1)+r':  '+pr.format(sum_terms0))
+                print(pad3+r'  + Delta e_'+'{:d}'.format(terms)+r':  '+pr.format(final_term))
+                print(pad3+r'  =       e_'+'{:d}'.format(terms)+r':  '+pr.format(sum_terms))
+                print('    e_infinity:  {}'.format(e))
+                print('rounding error:'+pad1+pr.format(rd))
+
+        # Configure callbacks and go!
+        for w in items:
+            w.observe(on_value_change, names='value')
 else:
     # Class and functions for "Fun with Floats" exercise
     class Fwf:
@@ -103,88 +166,88 @@ else:
         w = fwf.plot()
         display(w)
 
-# Class and functions for "Exponential" exercise
-class Exp:
-	def __init__(self, delta_e):
-		# save delta e function
-		self.delta_e = delta_e
-		
-		# create empty axes
-		self.fig,self.ax = plt.subplots(1,1)
-		self.fig.set_size_inches([9,3])
-		self.ax.axis('off')
-		self.ax.set_xlim([0,1])
-		self.ax.set_ylim([-0.40,1])
-		
-		# create empty text annotations
-		self.string = ''
-		self.text0 = self.ax.text(-0.135,0.90, '', size = 15, ha = 'left')
-		self.text1 = self.ax.text(-0.135,0.65, '', size = 15, ha = 'left')
-		self.text2 = self.ax.text(-0.105,0.4, '', size = 15, ha = 'left')
-		self.text3 = self.ax.text(-0.075,0.05, '', size = 15, ha = 'left', color = 'r', bbox=dict(facecolor='none', edgecolor='red', linestyle='--'))
-		
-		self.text4 = self.ax.text(-0.135,-0.3, '', size = 15, ha = 'left', color='b')
-				
-		# create widgets
-		items = [
-			IntSlider(value = 1, description='terms', min=1, max = 20, step=1),
-			Dropdown(options = ['half','single','double'], value = 'single')
-			]
-		self.widgets = items
-			
-		# set initial state
-		self.update(None)
-	def update(self, change):
-		# grab current precision from appropriate widget
-		precision = self.widgets[1].value
-		if precision == 'half':
-			prec = np.float16
-		elif precision == 'single':
-			prec = np.float32
-		else:
-			prec = np.float64
-			
-		# grab current term calculation from appropriate widget
-		terms = self.widgets[0].value
-		
-		# calculation
-		# -----------
-			# initialize term calculation
-		sum_terms0 = prec(0.)
-		final_term = prec(0.)		
-			# iterate over number of terms
-		for i in range(0,terms):
-			final_term = self.delta_e(i, prec)
-			sum_terms0 += final_term
-			# compute and append final term
-		final_term = self.delta_e(terms, prec)
-		sum_terms = sum_terms0 + final_term
-		
-		# update annotations
-		pr = '{:51.50f}'
-		self.text0.set_text(r'       e$_{'+'{:d}'.format(terms-1)+'}$'+r'$:$  '+pr.format(sum_terms0))
-		self.text1.set_text(r'  $+\Delta$e$_{'+'{:d}'.format(terms)+'}$'+r'$:$  '+pr.format(final_term))
-		self.text2.set_text(r'$=$e$_{'+'{:d}'.format(terms)+'}$'+r'$:$  '+pr.format(sum_terms))
-		e = '2.71828182845904523536028747135266249775724709369995'
-		self.text3.set_text('e$_\infty:$  {}'.format(e))
-				
-		rd = Decimal(pr.format(sum_terms))-(Decimal(pr.format(final_term))+Decimal(pr.format(sum_terms0)))
-		self.text4.set_text('err$_{round}:$'+'  '+pr.format(rd))
-		
-		# redraw
-		self.fig.canvas.draw()
-	def plot(self):
-		# check widgets for updates
-		for w in self.widgets:
-			w.observe(self.update, 'value')
-		self.update(None)
-		
-		# widget layout
-		col_layout=Layout(display='flex', flex_flow='row-wrap', width="100%", justify_content='center', align_items='center')
-		return HBox(self.widgets, layout=col_layout)
-	
-def exponential_example(delta_e):
-	exp = Exp(delta_e)
-	w = exp.plot()
-	display(w)
-	
+    # Class and functions for "Exponential" exercise
+    class Exp:
+        def __init__(self, delta_e):
+            # save delta e function
+            self.delta_e = delta_e
+            
+            # create empty axes
+            self.fig,self.ax = plt.subplots(1,1)
+            self.fig.set_size_inches([9,3])
+            self.ax.axis('off')
+            self.ax.set_xlim([0,1])
+            self.ax.set_ylim([-0.40,1])
+            
+            # create empty text annotations
+            self.string = ''
+            self.text0 = self.ax.text(-0.135,0.90, '', size = 15, ha = 'left')
+            self.text1 = self.ax.text(-0.135,0.65, '', size = 15, ha = 'left')
+            self.text2 = self.ax.text(-0.105,0.4, '', size = 15, ha = 'left')
+            self.text3 = self.ax.text(-0.075,0.05, '', size = 15, ha = 'left', color = 'r', bbox=dict(facecolor='none', edgecolor='red', linestyle='--'))
+            
+            self.text4 = self.ax.text(-0.135,-0.3, '', size = 15, ha = 'left', color='b')
+                    
+            # create widgets
+            items = [
+                IntSlider(value = 1, description='terms', min=1, max = 20, step=1),
+                Dropdown(options = ['half','single','double'], value = 'single')
+                ]
+            self.widgets = items
+                
+            # set initial state
+            self.update(None)
+        def update(self, change):
+            # grab current precision from appropriate widget
+            precision = self.widgets[1].value
+            if precision == 'half':
+                prec = np.float16
+            elif precision == 'single':
+                prec = np.float32
+            else:
+                prec = np.float64
+                
+            # grab current term calculation from appropriate widget
+            terms = self.widgets[0].value
+            
+            # calculation
+            # -----------
+                # initialize term calculation
+            sum_terms0 = prec(0.)
+            final_term = prec(0.)		
+                # iterate over number of terms
+            for i in range(0,terms):
+                final_term = self.delta_e(i, prec)
+                sum_terms0 += final_term
+                # compute and append final term
+            final_term = self.delta_e(terms, prec)
+            sum_terms = sum_terms0 + final_term
+            
+            # update annotations
+            pr = '{:51.50f}'
+            self.text0.set_text(r'       e$_{'+'{:d}'.format(terms-1)+'}$'+r'$:$  '+pr.format(sum_terms0))
+            self.text1.set_text(r'  $+\Delta$e$_{'+'{:d}'.format(terms)+'}$'+r'$:$  '+pr.format(final_term))
+            self.text2.set_text(r'$=$e$_{'+'{:d}'.format(terms)+'}$'+r'$:$  '+pr.format(sum_terms))
+            e = '2.71828182845904523536028747135266249775724709369995'
+            self.text3.set_text('e$_\infty:$  {}'.format(e))
+                    
+            rd = Decimal(pr.format(sum_terms))-(Decimal(pr.format(final_term))+Decimal(pr.format(sum_terms0)))
+            self.text4.set_text('err$_{round}:$'+'  '+pr.format(rd))
+            
+            # redraw
+            self.fig.canvas.draw()
+        def plot(self):
+            # check widgets for updates
+            for w in self.widgets:
+                w.observe(self.update, 'value')
+            self.update(None)
+            
+            # widget layout
+            col_layout=Layout(display='flex', flex_flow='row-wrap', width="100%", justify_content='center', align_items='center')
+            return HBox(self.widgets, layout=col_layout)
+        
+    def exponential_example(delta_e):
+        exp = Exp(delta_e)
+        w = exp.plot()
+        display(w)
+        
