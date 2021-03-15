ea84872f1830a53fe463df2eba3a6ff95e970de6
Bryan Ruddy <b.ruddy@auckland.ac.nz>
2021-03-15 16:48:12 +1300

    Finish lecture updates
    Update Jupyter notebook

--- quality_control/funcs.py
+++ quality_control/funcs.py
@@ -1,4 +1,6 @@
 import numpy as np
 
 def approx_factorial(x):
+    if x < 0:
+        raise ValueError
     return (2*np.pi*x)**0.5*(x/np.e)**x
--- quality_control/test_other.py
+++ quality_control/test_other.py
@@ -6,10 +6,12 @@ def test_answer():
 	assert approx_factorial(0.) < tol
     assert approx_factorial(1) - 0.9221370088957891 < tol
     assert approx_factorial(np.inf) == np.inf
+    assert approx_factorial(-np.inf) == 0
 	
 def test_answer2():
 	
-	assert iscomplex(approx_factorial(-1))
+	with pytest.raises(ValueError)
+        approx_factorial(-3)
     with pytest.raises(TypeError):
         approx_factorial('Colorless green ideas sleep furiously')
 	
