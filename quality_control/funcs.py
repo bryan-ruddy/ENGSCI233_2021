b74b1d11f3670aeaac6d4156119e8334cd26aef4
Bryan Ruddy <b.ruddy@auckland.ac.nz>
2021-03-15 16:40:09 +1300

    Add lecture examples
    Change function and tests for demo purposes

--- quality_control/funcs.py
+++ quality_control/funcs.py
@@ -1,2 +1,4 @@
-def func(x):
-    return x + 1
+import numpy as np
+
+def approx_factorial(x):
+    return (2*np.pi*x)**0.5*(x/np.e)**x
--- quality_control/test_other.py
+++ quality_control/test_other.py
@@ -1,13 +1,15 @@
 from funcs import *
+import pytest
+
 def test_answer():
-	''' details about
-		more details
-	'''
-	assert func(3) == 5	
+	tol=1e-10
+	assert approx_factorial(0.) < tol
+    assert approx_factorial(1) - 0.9221370088957891 < tol
+    assert approx_factorial(np.inf) == np.inf
 	
 def test_answer2():
-	''' details about
-		more details
-	'''
-	assert func(-1) == 0	
+	
+	assert iscomplex(approx_factorial(-1))
+    with pytest.raises(TypeError):
+        approx_factorial('Colorless green ideas sleep furiously')
 	
