a695964b0d14ae3037ae4269ba2048ed29ccc539
Bryan Ruddy <b.ruddy@auckland.ac.nz>
2021-03-15 16:50:18 +1300

    Update test_other.py
    Fix whitespace

--- quality_control/test_other.py
+++ quality_control/test_other.py
@@ -2,16 +2,14 @@ from funcs import *
 import pytest
 
 def test_answer():
-	tol=1e-10
-	assert approx_factorial(0.) < tol
+    tol=1e-10
+    assert approx_factorial(0.) < tol
     assert approx_factorial(1) - 0.9221370088957891 < tol
     assert approx_factorial(np.inf) == np.inf
     assert approx_factorial(-np.inf) == 0
-	
+
 def test_answer2():
-	
-	with pytest.raises(ValueError)
+    with pytest.raises(ValueError)
         approx_factorial(-3)
     with pytest.raises(TypeError):
         approx_factorial('Colorless green ideas sleep furiously')
-	
